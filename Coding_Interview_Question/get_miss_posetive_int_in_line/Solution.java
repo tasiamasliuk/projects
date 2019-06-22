59b4b2288cee57b11a0abc7e8133e26fd4161e8c
Tasia Masliuk <44476222+tasiamasliuk@users.noreply.github.com>
2019-06-23 00:55:11 +0200

    Update Solution.java

--- Coding_Interview_Question/get_miss_posetive_int_in_line/Solution.java
+++ Coding_Interview_Question/get_miss_posetive_int_in_line/Solution.java
@@ -5,24 +5,27 @@ public class Solution {
             posetive_int = arr[0]-1;
         }
         for (int i = 0; i < (arr.length - 1); i++) {
-            int temp_min = arr[i] + 1;
-            if (arr[i + 1] != temp_min){
-                if (temp_min < posetive_int)
+            if (arr[i] + 1 > 0 ) {
+                int temp_min = arr[i] + 1;
+                if (arr[i + 1] != temp_min) {
+                    //if (temp_min < posetive_int)
                     posetive_int = temp_min;
+                }
             }
         }
         if (arr[arr.length - 1]+1 > 0 && arr[arr.length - 1] > 0){
-            if (arr[arr.length - 1]+1 < posetive_int)
-                posetive_int = arr[0]-1;
+            if (arr[arr.length - 1]+1 < posetive_int && arr[arr.length - 1] > 0)
+                posetive_int = arr[arr.length - 1]+1;
         }
+
+
         return posetive_int;
     }
-    
     public static void main(String[] args) {
         int[] check_me_1 = {3,4,-1,1};
-        System.out.println("positive integer in line thet was missed: " + get_miss_posetive_in_line(check_me_1));
+        System.out.println("positive integer in line that was missed: " + get_miss_posetive_in_line(check_me_1));
 
         int[] check_me_2 = {1,2,0};
-        System.out.println("positive integer in line thet was missed: " + get_miss_posetive_in_line(check_me_2));
+        System.out.println("positive integer in line that was missed: " + get_miss_posetive_in_line(check_me_2));
     }
 }
