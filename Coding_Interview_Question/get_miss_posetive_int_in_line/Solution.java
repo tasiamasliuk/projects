e951e6ea45485b7c3be2eb24abaaa08fc488f712
Tasia Masliuk <44476222+tasiamasliuk@users.noreply.github.com>
2019-06-22 15:35:42 +0200

    Create Solution.java

---
+++ Coding_Interview_Question/get_miss_posetive_int_in_line/Solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public static int get_miss_posetive_in_line(int[] arr){
+        int posetive_int = arr[0];
+        if (arr[0]-1 > 0){
+            posetive_int = arr[0]-1;
+        }
+        for (int i = 0; i < (arr.length - 1); i++) {
+            int temp_min = arr[i] + 1;
+            if (arr[i + 1] != temp_min){
+                if (temp_min < posetive_int)
+                    posetive_int = temp_min;
+            }
+        }
+        if (arr[arr.length - 1]+1 > 0 && arr[arr.length - 1] > 0){
+            if (arr[arr.length - 1]+1 < posetive_int)
+                posetive_int = arr[0]-1;
+        }
+        return posetive_int;
+    }
+    
+    public static void main(String[] args) {
+        int[] check_me_1 = {3,4,-1,1};
+        System.out.println("positive integer in line thet was missed: " + get_miss_posetive_in_line(check_me_1));
+
+        int[] check_me_2 = {1,2,0};
+        System.out.println("positive integer in line thet was missed: " + get_miss_posetive_in_line(check_me_2));
+    }
+}
