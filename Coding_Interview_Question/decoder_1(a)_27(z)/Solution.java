a1ed8ae0761dd75e106f6ea30ece0e3f7bd7c063
Tasia Masliuk <44476222+tasiamasliuk@users.noreply.github.com>
2019-06-24 02:02:05 +0200

    Create Solution.java

---
+++ Coding_Interview_Question/decoder_1(a)_27(z)/Solution.java
@@ -0,0 +1,43 @@
+public class Solution {
+    public static int decoder(String data){
+        if (data.length() == 0){
+            System.out.println("PUSTO");
+            return 0;
+        }
+
+        if (Integer.parseInt(data) == 0){
+            System.out.println("IT IS 0!");
+            return 0;
+        }
+
+        if (0 < Integer.parseInt(data) && Integer.parseInt(data) <= 9){
+            System.out.println("0 < " + Integer.parseInt(data) + " <= 9");
+            return 1;
+        }
+
+
+        if (0 < Integer.parseInt(data.substring(0,2)) && Integer.parseInt(data.substring(0,2)) < 27){
+            System.out.println("decode " + data.substring(1) + " + " + data.substring(2));
+            return decoder(data.substring(1)) + decoder(data.substring(2));
+        }
+
+        else
+            if (0 < Integer.parseInt(data.substring(0,1)))
+            {
+                System.out.println("decode " + data.substring(1));
+                return decoder(data.substring(1));
+            }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String try_me_1 = "111";
+        System.out.println("There are " + decoder(try_me_1) + " ways to decode " + try_me_1);
+
+        String try_me_2 = "001";
+        System.out.println("There are " + decoder(try_me_2) + " ways to decode " + try_me_2);
+
+        String try_me_3 = "12345";
+        System.out.println("There are " + decoder(try_me_3) + " ways to decode " + try_me_3);
+    }
+}
