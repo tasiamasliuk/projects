725d8047a4d60fee6133e3b683da44197f6a2b6c
Tasia Masliuk <44476222+tasiamasliuk@users.noreply.github.com>
2019-06-24 11:38:47 +0200

    Update Solution.java

--- Coding_Interview_Question/decoder_1(a)_27(z)/Solution.java
+++ Coding_Interview_Question/decoder_1(a)_27(z)/Solution.java
@@ -1,33 +1,24 @@
 public class Solution {
     public static int decoder(String data){
-        if (data.length() == 0){
-            System.out.println("PUSTO");
+        if (data.length() == 0)
             return 0;
-        }
 
-        if (Integer.parseInt(data) == 0){
-            System.out.println("IT IS 0!");
+        if (Integer.parseInt(data) == 0)
             return 0;
-        }
 
-        if (0 < Integer.parseInt(data) && Integer.parseInt(data) <= 9){
-            System.out.println("0 < " + Integer.parseInt(data) + " <= 9");
+        if (0 < Integer.parseInt(data) && Integer.parseInt(data) <= 9 && data.length() == 1)
             return 1;
-        }
 
-
-        if (0 < Integer.parseInt(data.substring(0,2)) && Integer.parseInt(data.substring(0,2)) < 27){
-            System.out.println("decode " + data.substring(1) + " + " + data.substring(2));
+        if (9 < Integer.parseInt(data.substring(0,2)) && Integer.parseInt(data.substring(0,2)) < 27){
+            if (data.length() == 2)
+                return 1 + decoder(data.substring(1));
             return decoder(data.substring(1)) + decoder(data.substring(2));
         }
 
         else
             if (0 < Integer.parseInt(data.substring(0,1)))
-            {
-                System.out.println("decode " + data.substring(1));
                 return decoder(data.substring(1));
-            }
-        return -1;
+        return 0;
     }
 
     public static void main(String[] args) {
@@ -37,7 +28,10 @@ public class Solution {
         String try_me_2 = "001";
         System.out.println("There are " + decoder(try_me_2) + " ways to decode " + try_me_2);
 
-        String try_me_3 = "12345";
+        String try_me_3 = "2012";
         System.out.println("There are " + decoder(try_me_3) + " ways to decode " + try_me_3);
+
+        String try_me_4 = "12345";
+        System.out.println("There are " + decoder(try_me_3) + " ways to decode " + try_me_4);
     }
 }
